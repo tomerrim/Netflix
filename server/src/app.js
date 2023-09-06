1bbf9b9dae6bce9b6d5de0993b2afcc62b0b708c
Tomer <tomerrim@gmail.com>
2023-09-06 12:24:50 +0300

    added origin to cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,11 @@ import contentRouter from "./routes/contentRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tomer-netflix.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
